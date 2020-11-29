997a657c928f02cfafbcea3eef34fd6de5d2cf6d
buscaroli <m.buscaroli@outlook.com>
2020-11-29 18:24:05 +0000

    Added rendering of the gallery using GraphQL.

--- src/pages/pets.js
+++ src/pages/pets.js
@@ -1,14 +1,53 @@
 import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+import Img from 'gatsby-image'
+
 import Layout from '../components/layout'
 import petsStyles from './pets.module.scss'
 
 
 const Pets = () => {
+    const data = useStaticQuery(graphql`
+        query { 
+            allFile(filter: {relativePath: {}, sourceInstanceName: {eq: "pets"}}) 
+            {
+                nodes {
+                    childImageSharp {
+                        sizes {
+                            originalName
+                            src
+                        }
+                        fluid {
+                            ...GatsbyImageSharpFluid
+                        }
+                    }
+                    id
+                }
+            
+            }
+        }
+    `)
+
+    const imageList = data.allFile.nodes.map(picture => {
+        if (!picture) return ''
+
+        return (
+            <div key={ picture.id }className={petsStyles.gallery__div}>
+                {/* <img className={petsStyles.gallery__pic} src={picture.childImageSharp.fluid.src} alt='cucciolo' ></img>
+                 */}
+                 <Img className={petsStyles.gallery__pic} fluid={picture.childImageSharp.fluid} alt='cucciolo' />
+            </div>
+        )
+    })
+
 
     return (
         <Layout>
-            <div className={petsStyles.gallery}>
-                Pets Page
+            <div>
+                <div className={petsStyles.title}>I Nostri Cuccioli</div>
+                <div className={petsStyles.gallery}>
+                    {imageList} 
+                </div>
             </div>
         </Layout>
         
