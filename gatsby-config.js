d8374880fed41398b8b69aa9f1ce3e35d8b27915
buscaroli <m.buscaroli@outlook.com>
2020-11-29 18:28:41 +0000

    Fixing netlify deployment bugs.

--- gatsby-config.js
+++ gatsby-config.js
@@ -13,13 +13,6 @@ module.exports = {
     'gatsby-plugin-react-helmet',
     'gatsby-image',
     'gatsby-plugin-netlify-cms',
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     'gatsby-transformer-sharp',
     'gatsby-plugin-sharp',
     {
